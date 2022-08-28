c2347793aecbca9cd4a4a9aa215f7d7f7f4fb8ac
Vasiliy <vasiliy.trunov.00@mail.ru>
2022-08-28 21:08:03 +0200

    Задача 36

---
+++ program_036/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] ArrayGenerator(){
+    int number = new Random().Next(2, 11);
+    int[] array = new int[number];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-50, 51);
+    }
+    return array;
+}
+
+int LookForEvenIndex(int[] array){
+    int number  = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i%2 != 0)
+        {
+            number+=array[i];
+        }
+    }
+    return number;
+}
+
+int[] newArray = ArrayGenerator();
+int evenIndex = LookForEvenIndex(newArray);
+
+System.Console.WriteLine($"[{String.Join(',', newArray)}] -> {evenIndex}");
